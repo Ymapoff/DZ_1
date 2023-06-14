8d00f5cc1e589805ac5f024503ecacbddd1b5252
Ymapoff <kumar13-05@mail.ru>
2023-06-14 17:54:49 +0300

    junit4

--- src/test/java/CashbackHackServiceTest.java
+++ src/test/java/CashbackHackServiceTest.java
@@ -1,6 +1,6 @@
-import org.testng.annotations.Test;
+import org.junit.Test;
 
-import static org.testng.Assert.assertEquals;
+import static org.junit.Assert.assertEquals;
 
 public class CashbackHackServiceTest {
 
