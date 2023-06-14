ddde12cfe5e7c4032dbf846571fa25754649efd2
Ymapoff <kumar13-05@mail.ru>
2023-06-14 17:11:06 +0300

    testng

---
+++ src/test/java/CashbackHackServiceTest.java
@@ -0,0 +1,44 @@
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+
+public class CashbackHackServiceTest {
+
+    @Test
+    public void shouldCashback900() {
+        CashbackHackService service = new CashbackHackService();
+
+        int amount = 900;
+
+        int actual = service.remain(amount);
+        int expected = 100;
+
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void shouldCashback1200(){
+        CashbackHackService service = new CashbackHackService();
+
+        int amount = 1200;
+
+        int actual = service.remain(amount);
+        int expected = 800;
+
+        assertEquals(actual , expected);
+    }
+
+    @Test
+    public void shouldCashback1000(){
+        CashbackHackService service = new CashbackHackService();
+
+        int amount = 1000;
+
+
+        int actual = service.remain(amount);
+        int expected = 0;
+
+        assertEquals(actual , expected);
+    }
+
+}
